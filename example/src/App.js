db428be9f3603fd10d5bd90ac05ef591c749c24b
Ethan Selzer <ethanselzer@gmail.com>
2017-02-01 11:17:28 -0800

    Add example project

---
+++ example/src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import ReactCursorPosition from '../../dist/ReactCursorPosition';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cursorPosition: {
+                x: 0,
+                y: 0,
+                isOutside: true
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <ReactCursorPosition  {...{
+                    style: {
+                        width: '400px',
+                        height: '400px',
+                        margin: '0 auto',
+                        border: 'solid 1px green'
+                    },
+                    onCursorPositionChanged: (cursorPosition) => {
+                        this.setState({ cursorPosition });
+                    }
+                }}>
+                    <Label />
+                    <Label {...{
+                        mapCursorPositionToContent: cp => `y: ${cp.y}`
+                    }} />
+                    <Label {...{
+                        mapCursorPositionToContent: cp => `isOutside: ${ cp.isOutside ? 'true' : 'false'}`
+                    }} />
+                </ReactCursorPosition>
+                <Label {...this.state} />
+                <Label {...this.state} {...{
+                    mapCursorPositionToContent: cp => `y: ${cp.y}`
+                }} />
+                <Label {...this.state} {...{
+                    mapCursorPositionToContent: cp => `isOutside: ${ cp.isOutside ? 'true' : 'false'}`
+                }} />
+            </div>
+        );
+    }
+}
+
+export default App;
+
+const Label = (props) => {
+    return (
+        <div>
+            { props.mapCursorPositionToContent(props.cursorPosition) }
+        </div>
+    );
+}
+
+Label.defaultProps = {
+    mapCursorPositionToContent: cp => `x: ${cp.x}`
+}
