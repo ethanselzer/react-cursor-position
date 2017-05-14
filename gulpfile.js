e05a23138294bac138b3909245ac5ae4b57f5c29
Ethan Selzer <ethanselzer@gmail.com>
2017-05-14 12:51:30 -0700

    Prevent Test Watch Exit on Error

--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,44 @@ var coveralls = require('gulp-coveralls');
 var isparta = require('isparta');
 var runSequence = require('run-sequence');
 
-// Transform all required files with Babel
 process.env.BABEL_ENV = 'cjs';
 require('babel-core/register');
 
 var TEST_FILES = 'test/*.spec.js';
 var SRC_FILES = 'src/*.js';
 
+var isWatching = false;
+function onError(err) {
+    gutil.log(err);
+    if (isWatching) {
+        this.emit('end');
+    } else {
+        process.exit(1);
+    }
+}
+
 gulp.task('test', function () {
     return gulp.src(TEST_FILES, { read: false })
         .pipe(mocha({
-            require: [__dirname + '/scripts/jsdom'] // Prepare jsdom environement
-        }));
+            require: [__dirname + '/scripts/jsdom'], // Prepare jsdom environement
+            bail: false
+        }).on('error', onError));
 });
 
 gulp.task('coverage:instrument', function () {
     return gulp.src(SRC_FILES)
+        // https://github.com/SBoudrias/gulp-istanbul#istanbulopt
         .pipe(istanbul({
-            instrumenter: isparta.Instrumenter // Use the isparta instrumenter (code coverage for ES6)
-            // https://github.com/SBoudrias/gulp-istanbul#istanbulopt
+            instrumenter: isparta.Instrumenter
         }))
-        .pipe(istanbul.hookRequire()); // Force `require` to return covered files
         // https://github.com/SBoudrias/gulp-istanbul#istanbulhookrequire
+        .pipe(istanbul.hookRequire()); // Force `require` to return covered files
 });
 
 gulp.task('coverage:report', function () {
     return gulp.src(SRC_FILES, { read: false })
-        .pipe(istanbul.writeReports({
-            // https://github.com/SBoudrias/gulp-istanbul#istanbulwritereportsopt
-        }));
+        // https://github.com/SBoudrias/gulp-istanbul#istanbulwritereportsopt
+        .pipe(istanbul.writeReports());
 });
 
 gulp.task('test:coverage', function (done) {
@@ -52,9 +61,12 @@ gulp.task('test:coveralls', function (done) {
     runSequence('test:coverage', 'coveralls', done);
 });
 
-gulp.task('test:watch', ['test'], function () {
-    gulp.watch([
-        TEST_FILES,
-        SRC_FILES
-    ], ['test']).on('error', gutil.log);
+gulp.task('set:watching', function() {
+    isWatching = true;
 });
+
+gulp.task('watch:test', function() {
+    gulp.watch([TEST_FILES, SRC_FILES], ['test']);
+});
+
+gulp.task('test:watch', ['set:watching', 'test', 'watch:test']);
