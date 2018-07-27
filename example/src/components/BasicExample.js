58da642b0dbc6e696663c47e73320355fd925913
Ethan Selzer <ethanselzer@gmail.com>
2018-07-27 13:08:34 -0700

    wip constants

--- example/src/components/BasicExample.js
+++ example/src/components/BasicExample.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactCursorPosition from '../pkg-lnk/ReactCursorPosition';
+import { INTERACTIONS } from '../pkg-lnk/es/constants';
 
 import PositionLabel from './PositionLabel';
 import InstructionsLabel from './InstructionsLabel';
@@ -8,7 +9,8 @@ export default class extends React.Component {
     render() {
         return (
             <ReactCursorPosition {...{
-                className: 'example__target example__target--basic'
+                className: 'example__target example__target--basic',
+                activationInteraction: INTERACTIONS.TAP
             }}>
                 <PositionLabel />
                 <InstructionsLabel className="example__instructions" />
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,11 @@ export const PRESS_EVENT_TIMER_NAME = 'pressEvent';
 export const MOUSE_EMULATION_GUARD_TIMER_NAME = 'mouseEmulation';
 export const SET_ACTIVATION_TIMER_NAME = 'setHovering';
 export const UNSET_ACTIVATION_TIMER_NAME = 'unsetHovering';
+export const INTERACTIONS = {
+    TOUCH: 'touch',
+    TAP: 'tap',
+    DOUBLE_TAP: 'double_tap',
+    PRESS: 'press',
+    CLICK: 'click',
+    HOVER: 'hover'
+}
