e6b92b8ec5447469bae24e9c66e1fb74194f0329
Ethan Selzer <ethanselzer@gmail.com>
2018-08-07 00:28:42 -0700

    cleanup

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const PRESS_EVENT_TIMER_NAME = 'pressEvent';
+export const TAP_GESTURE_TIMER_NAME = 'tap';
 export const MOUSE_EMULATION_GUARD_TIMER_NAME = 'mouseEmulation';
 export const SET_ACTIVATION_TIMER_NAME = 'setHovering';
 export const UNSET_ACTIVATION_TIMER_NAME = 'unsetHovering';
