3edf8585e22b2bbb3855855cb059c363c75053dc
Ethan Selzer <ethanselzer@gmail.com>
2018-08-16 18:04:26 -0700

    wip jest

---
+++ test/setupj.js
@@ -0,0 +1,9 @@
+var Enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// module.exports = function() {
+//     console.log('hello setup', Adapter)
+//
+// }
