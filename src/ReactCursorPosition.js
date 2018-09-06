715bb571b954de773f1128fd76f4e27bc7d0ad11
Jacek Radko <jacekradko@gmail.com>
2018-09-06 14:20:31 -0500

    Do not declare isReactComponent

--- src/ReactCursorPosition.js
+++ src/ReactCursorPosition.js
@@ -430,14 +430,10 @@ export default class extends React.Component {
         return e.touches[0];
     }
 
-    isReactComponent(reactElement) {
-        return typeof reactElement.type === 'function';
-    }
-
     shouldDecorateChild(child) {
         return (
             !!child &&
-            this.isReactComponent(child) &&
+            typeof child.type === 'function' &&
             this.props.shouldDecorateChildren
         );
     }
